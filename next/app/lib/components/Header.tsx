ff453443a4db54afaa00f696f0dd2f5f84eeed60
jdtoombs <jdtoombs@gmail.com>
2025-02-19 15:04:00 -0800

    fix

--- next/app/lib/components/Header.tsx
+++ next/app/lib/components/Header.tsx
@@ -3,14 +3,14 @@ import React from "react";
 /** Basic Header component containing the BCGOV logo and title of the application. */
 export const Header: React.FC = () => {
   return (
-    <div className="w-full bg-primaryBlue flex flex-row items-center px-4">
+    <div className="w-full bg-primaryBlue flex flex-row items-center px-4 text-white">
       <img
         className="h-30 w-60 mr-4"
         src="/bcgov_white_text.png"
         alt="BC GOV logo"
       />
-      <span className="text-xl text-white">Zero-Emission Vehicles Reporting System</span>
-      <span className="ml-auto text-white">Government of British Columbia</span>
+      <span className="text-xl">Zero-Emission Vehicles Reporting System</span>
+      <span className="ml-auto">Government of British Columbia</span>
     </div>
   );
 };
