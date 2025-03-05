4ad2da60ad6f35695fc33e1222b1518e22d28ca0
tim738745 <tim.chen@gov.bc.ca>
2025-03-05 15:58:09 -0800

    tim - suggested changes

--- next/app/dashboard/page.tsx
+++ next/app/dashboard/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-
-import { useState } from "react";
 import { ContentCard } from "../lib/components";
+import { Suspense } from "react";
+import { LatestActivitySkeleton } from "./components/skeletons";
+import { LatestActivity } from "./components/latestActivity";
 
 export default function Dashboard() {
-  // TODO: Get latest activity from API
-  const [loading, _] = useState(true);
-
   return (
     <div className="flex flex-row w-full">
       <div className="flex flex-col w-1/3">
@@ -18,15 +15,9 @@ export default function Dashboard() {
         </ContentCard>
       </div>
       <ContentCard title="Latest activity" className="w-2/3 ml-2">
-        {loading ? (
-          <div className="animate-pulse space-y-2">
-            <div className="h-4 bg-gray-300 rounded"></div>
-            <div className="h-4 bg-gray-300 rounded"></div>
-            <div className="h-4 bg-gray-300 rounded"></div>
-          </div>
-        ) : (
-          <p>Latest activity goes here</p>
-        )}
+        <Suspense fallback={<LatestActivitySkeleton />}>
+          <LatestActivity />
+        </Suspense>
       </ContentCard>
     </div>
   );
