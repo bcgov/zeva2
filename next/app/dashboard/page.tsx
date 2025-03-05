627751c25686fc8282b58cff7bf7f3c759be84fd
jdtoombs <jdtoombs@gmail.com>
2025-03-05 06:01:08 -0800

    dashboard work

--- next/app/dashboard/page.tsx
+++ next/app/dashboard/page.tsx
@@ -1,13 +1,33 @@
-import { auth } from "@/auth";
-import { keycloakSignOut } from "@/app/lib/actions/keycloak";
-import { Button } from "@/app/lib/components/Button";
+"use client";
 
-export default async function Dashboard() {
-  const session = await auth();
+import { useState } from "react";
+import { ContentCard } from "../lib/components";
+
+export default function Dashboard() {
+  // TODO: Get latest activity from API
+  const [loading, _] = useState(true);
+
   return (
-    <div>
-      <h1>Hello {session?.user?.name}, you are logged in!</h1>
-      <Button onClick={keycloakSignOut}>Sign Out</Button>
+    <div className="flex flex-row w-full">
+      <div className="flex flex-col w-1/3">
+        <ContentCard title="Welcome">
+          <p>Welcome to the dashboard!</p>
+        </ContentCard>
+        <ContentCard title="We want to hear from you">
+          <p>What do you think of the dashboard?</p>
+        </ContentCard>
+      </div>
+      <ContentCard title="Latest activity" className="w-2/3 ml-2">
+        {loading ? (
+          <div className="animate-pulse space-y-2">
+            <div className="h-4 bg-gray-300 rounded"></div>
+            <div className="h-4 bg-gray-300 rounded"></div>
+            <div className="h-4 bg-gray-300 rounded"></div>
+          </div>
+        ) : (
+          <p>Latest activity goes here</p>
+        )}
+      </ContentCard>
     </div>
   );
 }
