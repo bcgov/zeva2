edb572f5fe1586290c8aea5bdd54393ac3503b10
tim738745 <tim.chen@gov.bc.ca>
2025-04-02 10:59:22 -0700

    zev unit transfer actions + transfer creation client component

---
+++ next/app/lib/utils/typeGuards.ts
@@ -0,0 +1,19 @@
+import { ModelYear, VehicleClass, ZevClass } from "@/prisma/generated/client";
+
+export const isVehicleClass = (s: string): s is VehicleClass => {
+  return Object.keys(VehicleClass).some((vehicleClass) => {
+    return vehicleClass === s;
+  });
+};
+
+export const isZevClass = (s: string): s is ZevClass => {
+  return Object.keys(ZevClass).some((zevClass) => {
+    return zevClass === s;
+  });
+};
+
+export const isModelYear = (s: string): s is ModelYear => {
+  return Object.keys(ModelYear).some((modelYear) => {
+    return modelYear === s;
+  });
+};
