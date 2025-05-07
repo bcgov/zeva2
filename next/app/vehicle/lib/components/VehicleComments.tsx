3116270b3cf7769d97cb38e551114577a97db9d5
emi-hi <emily.hillier@gmail.com>
2025-05-07 15:40:26 -0700

    feat: adds vehicle details page with history and comments

---
+++ next/app/vehicle/lib/components/VehicleComments.tsx
@@ -0,0 +1,30 @@
+import { getVehicleComments } from "../data";
+
+const VehicleComments = async (props: { id: number }) => {
+  const comments = await getVehicleComments(props.id);
+  if (comments && comments.length > 0) {
+    const entries = [];
+    for (const each of comments) {
+      entries.push(
+        <div key={each.id}>
+          <div>
+            <b>
+              {each.createTimestamp &&
+                each.createTimestamp &&
+                each.createTimestamp.toLocaleString("en-US", {
+                  timeZone: "America/Los_Angeles",
+                })}{" "}
+              {each.createUser}
+            </b>
+          </div>
+          {each.comment && <div>Comment: {each.comment}</div>}
+          <br />
+        </div>,
+      );
+    }
+    return <div>{entries}</div>;
+  }
+  return null;
+};
+
+export default VehicleComments;
