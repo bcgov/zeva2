25aeb237ad09de0a5906fac32bd4594b36b857dd
tim738745 <tim.chen@gov.bc.ca>
2025-05-08 10:46:54 -0700

    45: some minor changes

--- next/app/vehicle/lib/data.ts
+++ next/app/vehicle/lib/data.ts
@@ -57,10 +57,14 @@ export const getVehicles = async (
 
 export const getVehicleComments = async (vehicleId: number) => {
   const { userIsGov, userOrgId } = await getUserInfo();
-  const vehicle_comments = await prisma.vehicleComment.findMany({
-    where: {
-      vehicleId: vehicleId,
-    },
+  let whereClause: Prisma.VehicleCommentWhereInput = {
+    vehicleId: vehicleId,
+  };
+  if (!userIsGov) {
+    whereClause = { ...whereClause, vehicle: { organizationId: userOrgId } };
+  }
+  return await prisma.vehicleComment.findMany({
+    where: whereClause,
     include: {
       vehicle: {
         include: {
@@ -69,37 +73,34 @@ export const getVehicleComments = async (vehicleId: number) => {
       },
     },
   });
-  if (
-    vehicle_comments &&
-    vehicle_comments.length >= 1 &&
-    (userIsGov || userOrgId == vehicle_comments[0].vehicle.id)
-  ) {
-    return vehicle_comments;
-  }
-  return [];
 };
 
 export const getVehicle = async (vehicleId: number) => {
   const { userIsGov, userOrgId } = await getUserInfo();
-  const vehicle = await prisma.vehicle.findUnique({
-    where: {
-      id: vehicleId,
-    },
+  let whereClause: Prisma.VehicleWhereUniqueInput = {
+    id: vehicleId,
+  };
+  if (!userIsGov) {
+    whereClause = { ...whereClause, organizationId: userOrgId };
+  }
+  return await prisma.vehicle.findUnique({
+    where: whereClause,
     include: {
       organization: true,
     },
   });
-  if (vehicle && (userIsGov || userOrgId == vehicle.organization.id)) {
-    return vehicle;
-  }
 };
 
 export const getVehicleHistories = async (vehicleId: number) => {
   const { userIsGov, userOrgId } = await getUserInfo();
-  const vehicle_history = await prisma.vehicleChangeHistory.findMany({
-    where: {
-      vehicleId: vehicleId,
-    },
+  let whereClause: Prisma.VehicleChangeHistoryWhereInput = {
+    vehicleId: vehicleId,
+  };
+  if (!userIsGov) {
+    whereClause = { ...whereClause, vehicle: { organizationId: userOrgId } };
+  }
+  return await prisma.vehicleChangeHistory.findMany({
+    where: whereClause,
     include: {
       vehicle: {
         include: {
@@ -108,11 +109,4 @@ export const getVehicleHistories = async (vehicleId: number) => {
       },
     },
   });
-  if (
-    vehicle_history &&
-    vehicle_history.length >= 1 &&
-    (userIsGov || userOrgId == vehicle_history[0].vehicle.organization.id)
-  ) {
-    return vehicle_history;
-  }
 };
