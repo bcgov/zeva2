2878bf32260b9683cd8feed85b40aadbc47004e2
tim738745 <98717409+tim738745@users.noreply.github.com>
2025-05-22 12:38:20 -0700

    fix next build error (#99)

--- next/app/organizations/page.tsx
+++ next/app/organizations/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { LoadingSkeleton } from "../lib/components/skeletons";
-import { getPageParams, pageStringParams } from "../lib/utils/page";
+import { getPageParams, pageStringParams } from "../lib/utils/nextPage";
 import { OrganizationList } from "./lib/components/OrganizationList";
 
 const Page = async (props: {
