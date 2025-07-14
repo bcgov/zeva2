d36aec1b03f59d8c22ea0b2448a565035196a406
tim738745 <tim.chen@gov.bc.ca>
2025-07-14 10:00:13 -0700

    redis connection updates

--- next/bullmq/config.ts
+++ next/bullmq/config.ts
@@ -11,9 +11,11 @@ import { ConnectionOptions } from "bullmq";
 const connection: ConnectionOptions = {
   host: process.env.REDIS_HOST ?? "redis",
   port: parseInt(process.env.REDIS_PORT ?? "6379", 10),
-  username: process.env.REDIS_USERNAME ?? undefined,
   password: process.env.REDIS_PASSWORD ?? undefined,
-  tls: process.env.REDIS_TLS_DISABLED === "true" ? undefined : {},
+  tls:
+    process.env.REDIS_TLS_ENABLED === "true"
+      ? { rejectUnauthorized: false }
+      : undefined,
 };
 
 export const bullmqConfig = {
