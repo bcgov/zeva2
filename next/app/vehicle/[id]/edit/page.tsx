e1236f94e8edb9f2718a02d63b012fc7322ed90b
emi-hi <emily.hillier@gmail.com>
2025-07-23 11:12:39 -0700

    feat: adds filedrop to vehicle form so attachments can be added

--- next/app/vehicle/[id]/edit/page.tsx
+++ next/app/vehicle/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import { VehicleForm } from "../../lib/components/VehicleForm";
 import { notFound } from "next/navigation";
 import { getSerializedVehicle } from "../../lib/data";
-
+import { createOrUpdateVehicle } from "../../lib/actions";
 const Page = async (props: { params: Promise<{ id: string }> }) => {
   const args = await props.params;
   const id = parseInt(args.id);
@@ -10,10 +10,14 @@ const Page = async (props: { params: Promise<{ id: string }> }) => {
     notFound();
   }
 
+  const handleSave = async (data: VehiclePayload) => {
+    "use server";
+    await createOrUpdateVehicle(data);
+  };
   return (
     <div className="max-w-xl mx-auto p-4">
       <h1 className="text-xl font-bold mb-4">Edit Vehicle</h1>
-      <VehicleForm vehicle={vehicle} />
+      <VehicleForm vehicle={vehicle} handleSave={handleSave} />
     </div>
   );
 };
