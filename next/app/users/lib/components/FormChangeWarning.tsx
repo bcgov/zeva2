6df4df9df3a6de242c1e3e891c7acc32c2ff6c98
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2025-11-27 17:05:31 -0800

    feat: ZEVA-206 - Warning modal (#251)
    * Warning modal implemented
    
    * Implemented next-navigation-guard instead of custom hook
    
    * Sonar recommended cleanup

---
+++ next/app/users/lib/components/FormChangeWarning.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Modal } from "@/app/lib/components/Modal";
+import {
+  useCallback,
+} from "react";
+
+export const FormChangeWarning = (props: {
+  showWarningModal: boolean;
+  handleSaveAndNavigate: () => void;
+  handleNavigateWithoutSaving: () => void;
+  handleClose: () => void;
+  isPending: boolean;
+}) => {
+  // X button - just close the modal and stay on page
+  const handleClose = useCallback(() => {
+    props.handleClose();
+  }, [props]);
+
+  // "Leave without saving" button - close and navigate away
+  const handleLeaveWithoutSaving = useCallback(() => {
+    props.handleNavigateWithoutSaving();
+  }, [props]);
+
+  // "Save changes" button - save and navigate
+  const handleSaveAndLeave = useCallback(() => {
+    props.handleSaveAndNavigate();
+  }, [props]);
+
+  return (
+    <Modal
+      showModal={props.showWarningModal}
+      cancelLabel="Leave without saving"
+      confirmLabel="Save changes and exit"
+      modalType="warning"
+      handleCancel={handleLeaveWithoutSaving}
+      handleSubmit={handleSaveAndLeave}
+      title="Unsaved changes"
+      content={
+        <>
+          <div className="text-base font-bold mb-4">
+            Do you want to save your changes before exiting?
+          </div>
+          <div className="text-sm text-gray-600">
+            You've made changes to the user's information, but haven't saved them yet. You can save now and safely exit, or leave without saving.
+          </div>
+        </>
+      }
+      disablePrimaryButton={props.isPending}
+      disableSecondaryButton={props.isPending}
+      handleClose={handleClose}
+    />
+  );
+};
