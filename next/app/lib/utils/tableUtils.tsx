9b921cc05cebc5a5abfe9a47bd14acdccb4786bd
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2026-02-01 21:21:26 -0800

    feat: [ZEVA2-241] Client Side Table (#289)
    * Work in progress. Not thoroughly test yet
    
    * Attempt at fixing sonar issues. WIP
    
    * More fixes
    
    * Fixed dublication issue
    
    * Removed some duplicate code between the two tables
    
    * Made a table header component to remove duplication
    
    * Fixing sonar issues
    
    * Fixing sonar complexity issue
    
    * Fixed header issue

---
+++ next/app/lib/utils/tableUtils.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Table } from "@tanstack/react-table";
+
+/**
+ * Generates sizing headers for tables with explicit column sizing.
+ * This is used to enable column resizing functionality.
+ */
+export const getSizingHeaders = <T,>(
+  table: Table<T>,
+  explicitSizing: boolean,
+): React.JSX.Element | null => {
+  if (!explicitSizing) {
+    return null;
+  }
+  const lastGroup = table.getHeaderGroups().at(-1);
+  if (!lastGroup) {
+    return null;
+  }
+  const sizingHeaders: React.JSX.Element[] = lastGroup.headers.map(
+    (header) => (
+      <th
+        key={header.id}
+        className="px-6 py-3 border-b-2 relative"
+        style={{ width: header.getSize() }}
+      >
+        <div
+          {...{
+            onDoubleClick: () => header.column.resetSize(),
+            onMouseDown: header.getResizeHandler(),
+            className:
+              "bg-primaryBlue absolute w-[5px] h-full top-0 right-0 cursor-col-resize select-none touch-none",
+            style: {
+              transform: header.column.getIsResizing()
+                ? `translateX(${table.getState().columnSizingInfo.deltaOffset ?? 0}px)`
+                : "",
+            },
+          }}
+        />
+      </th>
+    ),
+  );
+  if (sizingHeaders) {
+    return <tr key={"sizingHeaders"}>{sizingHeaders}</tr>;
+  }
+  return null;
+};
