6b82599fccf40c7c9e1e276aa1b3ca485e35626c
Dallas Richmond <dallas.richmond@gov.bc.ca>
2026-02-11 15:58:08 -0800

    Updated seeded compliance data

--- next/prisma/seedProcesses/seedComplianceRatios.ts
+++ next/prisma/seedProcesses/seedComplianceRatios.ts
@@ -4,16 +4,23 @@ import { ModelYear } from "../generated/client";
 export const seedComplianceRatios = async (tx: TransactionClient) => {
   // Seed compliance ratios based on BC ZEV Act regulations
   const complianceRatios = [
-    { modelYear: ModelYear.MY_2024, complianceRatio: 26, zevClassA: 16 },
-    { modelYear: ModelYear.MY_2025, complianceRatio: 34, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2026, complianceRatio: 42, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2027, complianceRatio: 50, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2028, complianceRatio: 58, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2029, complianceRatio: 66, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2030, complianceRatio: 74, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2031, complianceRatio: 82, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2032, complianceRatio: 90, zevClassA: 20 },
-    { modelYear: ModelYear.MY_2033, complianceRatio: 100, zevClassA: 20 },
+    { modelYear: ModelYear.MY_2019, complianceRatio: 0, zevClassA: 0 },
+    { modelYear: ModelYear.MY_2020, complianceRatio: 9.5, zevClassA: 6 },
+    { modelYear: ModelYear.MY_2021, complianceRatio: 12, zevClassA: 8 },
+    { modelYear: ModelYear.MY_2022, complianceRatio: 14.5, zevClassA: 10 },
+    { modelYear: ModelYear.MY_2023, complianceRatio: 17, zevClassA: 12 },
+    { modelYear: ModelYear.MY_2024, complianceRatio: 19.5, zevClassA: 14 },
+    { modelYear: ModelYear.MY_2025, complianceRatio: 22, zevClassA: 16 },
+    { modelYear: ModelYear.MY_2026, complianceRatio: 26.3, zevClassA: 15.2 },
+    { modelYear: ModelYear.MY_2027, complianceRatio: 42.6, zevClassA: 28.7 },
+    { modelYear: ModelYear.MY_2028, complianceRatio: 58.9, zevClassA: 43.2 },
+    { modelYear: ModelYear.MY_2029, complianceRatio: 74.8, zevClassA: 58 },
+    { modelYear: ModelYear.MY_2030, complianceRatio: 91, zevClassA: 73.3 },
+    { modelYear: ModelYear.MY_2031, complianceRatio: 93.2, zevClassA: 77.2 },
+    { modelYear: ModelYear.MY_2032, complianceRatio: 95.2, zevClassA: 80.6 },
+    { modelYear: ModelYear.MY_2033, complianceRatio: 97.2, zevClassA: 83.7 },
+    { modelYear: ModelYear.MY_2034, complianceRatio: 99.3, zevClassA: 86.7 },
+    { modelYear: ModelYear.MY_2035, complianceRatio: 100, zevClassA: 89.5 },
   ];
 
   for (const ratio of complianceRatios) {
