7848f4ea0263758c5e2838b02c1c236fc1e2b23d
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2026-03-05 10:10:19 -0800

    fix: updated styling for nav (#381)
    * fix: updated styling for nav
    
    * fix: updated colours
    
    * fix: double tab activation styling

--- next/app/credit-application/lib/components/Navbar.tsx
+++ next/app/credit-application/lib/components/Navbar.tsx
@@ -11,17 +11,17 @@ export const Navbar = (props: {
 }) => {
   const searchParams = useSearchParams();
   return (
-    <div className="mb-4 flex gap-2 border-b">
+    <div className="mb-4 flex gap-2 border-b border-gray-400">
       <Link
         key={"records"}
-        className={`px-3 py-2 text-sm font-medium border-b-2 -mb-px ${props.slug === "validated" ? "border-blue-600 text-blue-700" : "border-transparent text-gray-600 hover:text-gray-900"}`}
+        className={`px-4 py-2.5 text-sm -mb-px rounded-t ${props.slug === "validated" ? "border-t border-l border-r border-gray-400 border-b-white bg-white text-black" : "border-t border-l border-r border-gray-300 border-b-gray-400 text-[#255A90] hover:bg-[#F7F9FC]"}`}
         href={`${Routes.CreditApplication}/${props.creditApplicationId}/validated?${searchParams.toString()}`}
       >
         All Records
       </Link>
       <Link
         key={"mismatches"}
-        className={`px-3 py-2 text-sm font-medium border-b-2 -mb-px ${props.slug === "model-name-mismatches" ? "border-blue-600 text-blue-700" : "border-transparent text-gray-600 hover:text-gray-900"}`}
+        className={`px-4 py-2.5 text-sm -mb-px rounded-t ${props.slug === "model-name-mismatches" ? "border-t border-l border-r border-gray-400 border-b-white bg-white text-black" : "border-t border-l border-r border-gray-300 border-b-gray-400 text-[#255A90] hover:bg-[#F7F9FC]"}`}
         href={`${Routes.CreditApplication}/${props.creditApplicationId}/model-name-mismatches?${searchParams.toString()}`}
       >
         Review Model Mismatches
