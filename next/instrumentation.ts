787785e1bb26b9ca5d8e13563c6d2aa15a1ef482
Dallas Richmond <dallas.richmond@gov.bc.ca>
2026-03-17 09:25:01 -0700

    fix: removed temp fix for personal local

--- next/instrumentation.ts
+++ next/instrumentation.ts
@@ -2,8 +2,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const imports = await import("./instrumentation-node");
     await imports.createBucket();
-    if (process.env.START_WORKERS === "true") {
-      await imports.upsertJobScheduler();
-    }
+    await imports.upsertJobScheduler();
   }
 }
