86cb579d1eaaf4360dd7c237f54cb30b584b7469
Dallas Richmond <dallas.richmond@gov.bc.ca>
2026-03-16 19:18:44 -0700

    fix: styling fix wip

--- next/instrumentation.ts
+++ next/instrumentation.ts
@@ -2,6 +2,8 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const imports = await import("./instrumentation-node");
     await imports.createBucket();
-    await imports.upsertJobScheduler();
+    if (process.env.START_WORKERS === "true") {
+      await imports.upsertJobScheduler();
+    }
   }
 }
