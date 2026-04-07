94263da01f19a96b2d4fe0599894a38c47978424
tim738745 <tim.chen@gov.bc.ca>
2026-04-07 05:08:10 -0700

    seventh commit

--- next/app/lib/components/Table.tsx
+++ next/app/lib/components/Table.tsx
@@ -78,18 +78,6 @@ export const Table = <T extends ZevaObject>({
         newParams.set(param, paramValue);
       }
     });
-
-    const currentFilters = searchParams.get("filters");
-    if (currentFilters) {
-      const filtersObject = getObject(currentFilters);
-      if (filtersObject.isActive) {
-        newParams.set(
-          "filters",
-          getString({ isActive: filtersObject.isActive }),
-        );
-      }
-    }
-
     replaceUrl(newParams);
   }, [paramsToPreserve, searchParams, replaceUrl]);
 
