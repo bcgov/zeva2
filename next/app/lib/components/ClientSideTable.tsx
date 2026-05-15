422ad0f8a8f5a46ab5af8c8af1289ecf9f1c53cc
julianforeman <julianforeman@me.com>
2026-05-15 12:09:00 -0700

    Updating table component placeholder text

--- next/app/lib/components/ClientSideTable.tsx
+++ next/app/lib/components/ClientSideTable.tsx
@@ -129,7 +129,7 @@ export const ClientSideTable = <T extends ZevaObject>({
                 />
                 <input
                   type="text"
-                  placeholder="Example text"
+                  placeholder="Search.."
                   value={globalFilter}
                   onChange={(e) => setGlobalFilter(e.target.value)}
                   className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
