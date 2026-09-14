86423e07bf1d81910a3b5c548634ecb910675203
tim738745 <tim.chen@gov.bc.ca>
2026-09-14 00:43:44 -0700

    feat: fifth commit

--- next/app/notifications/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { InAppNotificationType } from "@/prisma/generated/enums";
-
-export const isNotificationType = (s: string): s is InAppNotificationType => {
-  return Object.keys(InAppNotificationType).some((type) => {
-    return type === s;
-  });
-};
